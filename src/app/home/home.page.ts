898bd840f5837c795f81ba58cba146814c720808
kkeat <sp.dd.keat@gmail.com>
2020-06-04 23:02:13 +0800

    Partial completed UI, able to code with UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -6,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
+  async setPassengers() {
+    const alert = await this.alertController.create({
+      header: "How many going?",
+      inputs: [
+        {
+          name: "passengers",
+          type: "number",
+        }
+      ],
+      buttons: [
+        {
+          text: "Set",
+          handler: (e) => {
+            this.updatePassenger(e.passengers);
+          },
+
+        }, {
+          text: "Cancel",
+          role: "cancel"
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  updatePassenger(count) {
+    const passengerCountElement = document.getElementById("passenger_count");
+    passengerCountElement.innerHTML = count;
+  }
+
+  setPickupTime() {
+    document.getElementById("datetime").click();
+  }
+
+  calculateFare() {
+    const pickupDateElement = document.getElementById("datetime");
+    const passengerCountElement = document.getElementById("passenger_count");
+    const routeDistanceElement = document.getElementById("route_distance");
+  }
 }
